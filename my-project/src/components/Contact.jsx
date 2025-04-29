370515365013ed73192916292db24f902b1a794d
Yagyansh Singh Deshwal <yagyansh.singh.cse.2022@miet.ac.in>
2025-04-29 18:04:37 +0530

    build

--- my-project/src/components/Contact.jsx
+++ my-project/src/components/Contact.jsx
@@ -21,14 +21,29 @@ const Contact = () => {
         >
           {CONTACT.address}
         </motion.p>
-        <motion.p
+
+        <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
           transition={{ duration: 0.5 }}
           className="my-4"
         >
-          {CONTACT.phoneNo}
-        </motion.p>
+          <a
+            href={`tel:${CONTACT.phoneNo}`}
+            className="text-blue-500 hover:underline block"
+          >
+            Call: {CONTACT.phoneNo}
+          </a>
+          <a
+            href={`https://wa.me/${CONTACT.phoneNo.replace(/\D/g, "")}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-green-500 hover:underline"
+          >
+            Message on WhatsApp
+          </a>
+        </motion.div>
+
         <a
           href={`mailto:${CONTACT.email}`}
           className="text-blue-500 hover:underline"
